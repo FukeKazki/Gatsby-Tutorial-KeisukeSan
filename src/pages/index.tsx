e43d3cf109e3b5597119e20c5d2b9f93a89d04bf
FukeKazki <kazkichi0906@gmail.com>
2021-12-15 00:28:55 +0900

    Add: graphqlでデータ取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import * as React from "react"
+import {graphql} from "gatsby";
 
 // markup
-const IndexPage = () => {
+const IndexPage = ({ data }: any) => {
   return (
     <main>
       <p>hello gatsby</p>
+      {JSON.stringify(data)}
     </main>
   )
 }
+
+export const query = graphql`
+  {
+    site {
+      siteMetadata {
+        siteUrl
+        title
+      }
+    }
+  }
+`
 
 export default IndexPage
